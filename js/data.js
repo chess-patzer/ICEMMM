625b32c25bc4789936c6535518772b5ac2bfea08
chess-patzer <vishwanath14120@mech.ssn.edu.in>
2017-08-29 07:08:46 +0530

    Beta
    Site Uploaded

---
+++ js/data.js
@@ -0,0 +1,5 @@
+var codropsEvents = {
+    '01-27-2017' : '<h10>Last Date - IEEE India Council SAC</h10> <br><br> <div class = "event-paragraph">Last date to apply to be a part of the student co-ordination team for the 2017 IEEE India Council SAC.<br><a href = "http://bit.do/IC_SAC_2017" style = "font-size:100%; text-align: center;">Register here</a></div>',
+    
+    '01-31-2017' : '<h10>Last Date - Madras Section Volunteers</h10> <br><br> <div class = "event-paragraph">Last date to apply to be a volunteer for Madras Section<br><a href = "https://goo.gl/Dglf2T" style = "font-size:100%; text-align: center;">Register here</a></div>'
+};
